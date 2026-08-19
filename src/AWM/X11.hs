4a5a36bfd4e520ad3252d9a4ac4186301caf50a3
confucius40 <git@mail.katon15.xyz>
2026-08-19 12:23:12 +0200

    refactor(X11): make it translate events into changes in world

--- src/AWM/X11.hs
+++ src/AWM/X11.hs
@@ -2,23 +2,38 @@ module AWM.X11
   ( run
   ) where
 
+import AWM.World
 import Data.Bits ((.|.))
 import Graphics.X11
 import Graphics.X11.Xlib.Extras
-import Foreign.Marshal.Alloc (alloca)
+import Control.Monad (when)
 
 run :: IO ()
 run = do
   dpy <- openDisplay ""
   root <- rootWindow dpy (defaultScreen dpy)
-  selectInput dpy root (structureNotifyMask .|. substructureRedirectMask)
+  selectInput dpy root mask
   sync dpy False
-  loop dpy
+  loop dpy root empty
 
-loop :: Display -> IO ()
-loop dpy = do
+mask :: EventMask
+mask =
+  substructureRedirectMask
+  .|. substructureNotifyMask
+
+loop :: Display -> Window -> World -> IO ()
+loop dpy root world = do
   ev <- allocaXEvent $ \ptr -> do
     nextEvent dpy ptr
     getEvent ptr
-  print ev
-  loop dpy
+  case ev of
+    MapRequestEvent { ev_window = win } -> do
+      mapWindow dpy win
+      sync dpy False
+      loop dpy root (add (Win win) world)
+
+    DestroyWindowEvent { ev_window = win } ->
+      loop dpy root (del win world)
+
+    _ ->
+      loop dpy root world
