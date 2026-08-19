987d3ab6ae3b3be6c098def348b466b8b0edc8bc
confucius40 <git@mail.katon15.xyz>
2026-08-19 12:40:13 +0200

    add many shit

--- src/AWM/X11.hs
+++ src/AWM/X11.hs
@@ -6,7 +6,6 @@ import AWM.World
 import Data.Bits ((.|.))
 import Graphics.X11
 import Graphics.X11.Xlib.Extras
-import Control.Monad (when)
 
 run :: IO ()
 run = do
@@ -31,6 +30,29 @@ loop dpy root world = do
       mapWindow dpy win
       sync dpy False
       loop dpy root (add (Win win) world)
+
+    ConfigureRequestEvent
+      { ev_window = win
+      , ev_x = x
+      , ev_y = y
+      , ev_width = w
+      , ev_height = h
+      , ev_border_width = bw
+      , ev_value_mask = vm
+      } -> do
+        let changes =
+              WindowChanges
+                { wc_x = x
+                , wc_y = y
+                , wc_width = w
+                , wc_height = h
+                , wc_border_width = bw
+                , wc_sibling = 0
+                , wc_stack_mode = 0
+                }
+        configureWindow dpy win vm changes
+        sync dpy False
+        loop dpy root world
 
     DestroyWindowEvent { ev_window = win } ->
       loop dpy root (del win world)
